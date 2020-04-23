de81f601ecc417da76dd553b7971dff0d264b317
Gaurav <gauravgkm5@gmail.com>
2020-04-23 20:23:33 +0530

    Creating a servlet

---
+++ src/main/java/gaurav/helloWorldApplication.java
@@ -0,0 +1,12 @@
+package gaurav;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class helloWorldApplication {
+    public static void main(String[] args) {
+        System.out.println(100);
+        SpringApplication.run(helloWorldApplication.class, args);
+    }
+}
